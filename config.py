3bb606d3f63bcc27e7ba359e132e1eaed8237bf1
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-09-30 08:15:15 -0700

    السلام على آل بيت النبوة

---
+++ config.py
@@ -0,0 +1,8 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+SESSION = os.environ.get("SESSION")
+ha313so = TelegramClient(StringSession(session), APP_ID, APP_HASH)
+ha313so.start()
---
+++ haso.py
@@ -0,0 +1,113 @@
+import asyncio
+import logging
+from telethon import events
+from config import *
+from asyncio import sleep
+
+LOGS = logging.getLogger(__name__)
+
+yaAli = False
+async def aljoker_nshr(ha313so, sleeptimet, chat, message, seconds):
+    global yaAli
+    yaAli = True
+    while yaAli:
+        if message.media:
+            sent_message = await ha313so.send_file(chat, message.media, caption=message.text)
+        else:
+            sent_message = await ha313so.send_message(chat, message.text)
+        await asyncio.sleep(sleeptimet)
+@ha313so.on(events.NewMessage(outgoing=False, pattern='.نشر'))
+async def Hussein(event):
+    await event.delete()
+    parameters = re.split(r'\s+', event.text.strip(), maxsplit=2)
+    if len(parameters) != 3:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    seconds = int(parameters[1])
+    chat_usernames = parameters[2].split()
+    ha313so = event.client
+    global yaAli
+    yaAli = True
+    message = await event.get_reply_message()
+    for chat_username in chat_usernames:
+        try:
+            chat = await ha313so.get_entity(chat_username)
+            await aljoker_nshr(ha313so, seconds, chat.id, message, seconds)  # تمرير قيمة seconds هنا لكل مجموعة
+        except Exception as e:
+            await edit_delete(
+                event, f"⌔∮ لا يمكن العثور على المجموعة أو الدردشة {chat_username}: {str(e)}"
+            )
+        await asyncio.sleep(1)
+    
+async def aljoker_allnshr(ha313so, sleeptimet, message):
+    global yaAli
+    yaAli = True
+    aljoker_chats = await ha313so.get_dialogs()
+    while yaAli:
+        for chat in aljoker_chats:
+            if chat.is_group:
+                try:
+                    if message.media:
+                        await ha313so.send_file(chat.id, message.media, caption=message.text)
+                    else:
+                        await ha313so.send_message(chat.id, message.text)
+                except Exception as e:
+                    print(f"Error in sending message to chat {chat.id}: {e}")
+        await asyncio.sleep(sleeptimet)
+@ha313so.on(events.NewMessage(outgoing=False, pattern='.نشر_كروبات'))
+async def Hussein(event):
+    await event.delete()
+    seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    message =  await event.get_reply_message()
+    try:
+        sleeptimet = int(seconds[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    ha313so = event.client
+    global yaAli
+    yaAli = True
+    await aljoker_allnshr(ha313so, sleeptimet, message)
+super_groups = ["super", "سوبر"]
+async def aljoker_supernshr(ha313so, sleeptimet, message):
+    global yaAli
+    yaAli = True
+    aljoker_chats = await ha313so.get_dialogs()
+    while yaAli:
+        for chat in aljoker_chats:
+            chat_title_lower = chat.title.lower()
+            if chat.is_group and any(keyword in chat_title_lower for keyword in super_groups):
+                try:
+                    if message.media:
+                        await ha313so.send_file(chat.id, message.media, caption=message.text)
+                    else:
+                        await ha313so.send_message(chat.id, message.text)
+                except Exception as e:
+                    print(f"Error in sending message to chat {chat.id}: {e}")
+        await asyncio.sleep(sleeptimet)
+@ha313so.on(events.NewMessage(outgoing=False, pattern='.سوبر'))
+async def Hussein(event):
+    await event.delete()
+    seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    message =  await event.get_reply_message()
+    try:
+        sleeptimet = int(seconds[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    ha313so = event.client
+    global yaAli
+    yaAli = True
+    await aljoker_supernshr(ha313so, sleeptimet, message)
+@ha313so.ar_cmd(pattern="ايقاف (النشر|نشر)")
+async def stop_aljoker(event):
+    global yaAli
+    yaAli = False
+    await event.edit("**᯽︙ تم ايقاف النشر التلقائي بنجاح ✓** ")
+print ('تم تشغيل البوت')
+
+		
+ha313so.run_until_disconnected()
