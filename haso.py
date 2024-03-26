d67fe6e10c1d25e748798fd110b879b998274c59
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-03-27 01:10:23 +0300

    Update haso.py

--- haso.py
+++ haso.py
@@ -137,48 +137,6 @@ async def stop_aljoker(event):
     global yaAli
     yaAli = False
     await event.edit("**᯽︙ تم ايقاف النشر التلقائي بنجاح ✓** ")
-@ha313so.on(events.NewMessage(outgoing=True, pattern='.الذاتية تفعيل'))
-async def enable_hussein(event):
-    global hussein_enabled
-    hussein_enabled = True
-    await event.edit("**᯽︙تم تفعيل ميزة حفظ الذاتيات بنجاح ✓**")
-
-@ha313so.on(events.NewMessage(outgoing=True, pattern='.الذاتية تعطيل'))
-async def disable_hussein(event):  
-    global hussein_enabled 
-    hussein_enabled = False
-    await event.edit("**᯽︙تم تعطيل حفظ الذاتيات بنجاح ✓**")
-
-def joker_unread_media(message):
-    return message.media_unread and (message.photo or message.video)
-
-async def Hussein(event, caption):
-    media = await event.download_media()
-    sender = await event.get_sender()
-    sender_id = event.sender_id
-    lMl10l_date = event.date.strftime("%Y-%m-%d")
-    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
-    await bot.send_file(
-        "me",
-        media,
-        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
-        parse_mode="markdown"
-    )
-    os.remove(media)
-
-@ha313so.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_media(e)))
-async def Reda(event):
-    if hussein_enabled:
-        caption = """**
-           ♡  غير مبري الذمة اذا استعملته للأبتزاز  ♡
-♡ تم حفظ الذاتية بنجاح ✓
-♡ تم الصنع : @Jepthon
-♡ أسم المرسل : [{0}](tg://user?id={1})
-♡  تاريخ الذاتية : `{2}`
-♡  أرسلت في يوم `{3}`
-       ♡    ALJOKER    ♡
-        **"""
-        await Hussein(event, caption)
 
 @ha313so.on(events.NewMessage(outgoing=True, pattern=r"^\.(الاوامر|فحص)$"))
 async def Hussein(event):
